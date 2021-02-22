80f8676935498429eaa3b8fa35c9ed6dc61a154c
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-22 17:57:37 +0100

    algorithm 02 code has been modified

---
+++ algorithm02.py
@@ -0,0 +1,21 @@
+# algorithm 02
+# Complete the simpleArraySum function and It must return the sum of the array elements as an integer
+# The code starts here:
+
+import numpy as np
+
+# Taking the array input
+n = int (input())
+
+# Appending the user input in the list sequentially
+A = list (map(int,input().strip().split())) [:n]
+
+# Defining the function
+def simpleArraySum ():
+    for i in range (0, n):
+       sum_array = np.sum (A)
+    print (sum_array)
+    
+# Calling the function
+simpleArraySum ()
+    

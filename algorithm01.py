a91bf806df617c1c4af5ff998dff263eb7c39788
Sabrina Chowdhury <50597189+sabrina-oshin@users.noreply.github.com>
2021-02-22 17:13:22 +0100

    algorithm 01 code has been modified

--- algorithm01.py
+++ algorithm01.py
@@ -1,6 +1,6 @@
 # algorithm 01
 # Complete the function solveMeFirst to compute the sum of two integers
-# Contraints: 1 < a, b < 1000
+# Contraints: 1 <= a, b <= 1000
 # The code starts here:
 
 # Initialization
@@ -9,15 +9,15 @@ b = int (input("b = "))
 sum_out = 0
 
 # Defining a function
-def solveMeFirst (a,b):
-  if a and b > 1 and a and b < 1000:
+def solveMeFirst ():
+  if a and b >=  1 and a and b <= 1000:
      sum_out = a+b
   else:
        return 0
   print (sum_out)
 
 # Calling the function
-solveMeFirst (a,b)
+solveMeFirst ()
 
 
 
